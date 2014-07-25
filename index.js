74aee842b83cc4c79b54e62072e7a73af8b4ca4e
Christian G. Warden <cwarden@xerus.org>
2014-07-25 14:35:03 -0700

    Howl

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var meSpeak = require("mespeak");
 
 //Select english/american voice
-meSpeak.loadVoice(require("mespeak/voices/en/en-us.json"))
+meSpeak.loadVoice(require("mespeak/voices/en/en-us.json"));
 
 module.exports.play = function() {
-	meSpeak.speak("I am a wolf");
+	meSpeak.speak('I am a wolf. Awwwwooooo');
 };
